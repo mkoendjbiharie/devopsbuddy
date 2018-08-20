c5e2ac0ec2f0638e436b37650b20e5d0cb7f3d6f
Marlon Koendjbiharie <m.koendjbiharie@gmail.com>
2018-08-20 03:46:20 -0300

    Logging slf4j added to project Devopsbuddy

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.devopsbuddy.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -8,16 +10,21 @@ import java.util.Locale;
 
 @Service
 public class I18NService {
+    
+    /** the application logger */
+    private static final Logger LOG= LoggerFactory.getLogger(I18NService.class);
 
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId){
+        LOG.info("Returning I18n text for messageId {}",messageId);
         Locale locale=LocaleContextHolder.getLocale();
         return getMessage(messageId,locale);
     }
 
     public String getMessage(String messageId,Locale locale){
+
         return messageSource.getMessage(messageId,null,locale);
     }
 }
